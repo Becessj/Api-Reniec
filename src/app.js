1a31af053bc14034c4183d6bc2ba63ce824eac0e
Becessj <144996@unsaac.edu.pe>
2024-07-08 12:25:28 -0500

    updatin

--- src/app.js
+++ src/app.js
@@ -1,22 +1,3 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import helmet from "helmet";
-import router from "./routes/reniecsunat.routes";
-import morgan from 'morgan';
 
-// Initializations
-dotenv.config();
-const app = express();
-// Settings
-app.set("port", process.env.PORT || 3000);
-// Middlewares
-app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"))
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// Routes
-app.use(router);
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import app from './app.js';
-import {PORT} from './config.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import router from "./routes/reniecsunat.routes";
+import morgan from 'morgan';
+import {PORT} from './config';
+
+// Initializations
+dotenv.config();
+const app = express();
+// Settings
+app.set("port", process.env.PORT || 3000);
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// Routes
+app.use(router);
 
 // Starting the server
 app.listen(PORT, () => {
