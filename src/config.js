7a241d37cd221eb781b7681263d9550c5fe587af
Becessj <144996@unsaac.edu.pe>
2024-10-02 17:34:32 -0500

    prueba production

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ import {config} from 'dotenv';
 config()
 
 export const PORT = process.env.PORT || 3000
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'user_reniec'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '(-ZhL#9?9I5O'
+export const DB_HOST = process.env.DB_HOST || '50.62.220.142'
 export const DB_DATABASE = process.env.DB_DATABASE || 'reniecsunat'
 export const DB_PORT = process.env.DB_PORT || 3306
 
