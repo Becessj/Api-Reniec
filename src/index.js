fd0e0940f123f087695f7ee55bbaef8a3aab4c02
Becessj <144996@unsaac.edu.pe>
2025-03-06 14:46:31 -0500

    all origins 3

--- src/index.js
+++ src/index.js
@@ -25,7 +25,8 @@ const corsOptions = {
     'https://santiagolimpio.guamanpoma.org',
     'http://localhost:3000',
     'http://10.0.0.193:8082',
-    'http://10.0.0.193/Sistema_MesaPartes_GPA/tramite_nuevo.php'
+    'http://10.0.0.193/Sistema_MesaPartes_GPA/tramite_nuevo.php',
+    'https://especializacionseguridadalimentaria.guamanpoma.org'
   ], // Lista de orígenes permitidos
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
   allowedHeaders: ['Content-Type', 'Authorization', "x-access-token"], // Headers permitidos
